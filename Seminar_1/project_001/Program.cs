70d201759db94a2c15370cdf3bb0242793b8f871
Olga Torres <bestlaly@mail.ru>
2022-08-30 21:03:39 +0300

    Project_001

---
+++ Seminar_1/project_001/Program.cs
@@ -0,0 +1,9 @@
+﻿// квадрат числа
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int result = number*number;
+Console.WriteLine($"Квадрат числа {number} = {result}");
+
+//через библиотеку
+int result_2 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа {number} = {result_2}");
