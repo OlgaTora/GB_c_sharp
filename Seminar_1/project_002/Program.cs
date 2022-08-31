798cd58c6ff4e44a418e5b7a371863233207d70a
Olga Torres <bestlaly@mail.ru>
2022-08-31 11:13:07 +0300

    added tasks 2 and 4

--- Seminar_1/project_002/Program.cs
+++ Seminar_1/project_002/Program.cs
@@ -1,14 +1,54 @@
-﻿// Console.WriteLine("Введите число");
+﻿//Task 2
+//напишите программу, которая на вход принимает два числа и выдает, какое большее (max = 7)
+
+// Console.Write("a = ");
+// int a = int.Parse(Console.ReadLine());
+// Console.Write("b = ");
+// int b = int.Parse(Console.ReadLine());
+
+// if (a > b)
+// {
+//     Console.WriteLine($"max = {a}");
+// }
+// else
+// {
+//     Console.WriteLine($"max = {b}");
+// }
+
+//Task 4
+//напишите программу, которая на вход принимает три числа и выдает максимум из них (1,2,3 - 3)
+
+// Console.WriteLine("Введите три числа через enter");
+// int a = int.Parse(Console.ReadLine()),
+//     b = int.Parse(Console.ReadLine()),
+//     c = int.Parse(Console.ReadLine()); 
+// int max = a;
+
+// if(b > max)
+// {
+//     max = b;
+// }
+// if (c > max)
+// {
+//     max = c;
+// }
+// Console.WriteLine(max);
+
+//Task 6
+//напишите программу, которая на вход принимает число и выдает, является ли оно четным (да, нет)
+
+Console.WriteLine("Введите три числа через enter");
+int a = int.Parse(Console.ReadLine()),
+
+//Task 8
+//напишите программу, которая на вход принимает число N, а на выходе показывает
+//все четные числа от 1 до N (2, 4, 6)
+
+// Console.WriteLine("Введите число");
 // int number = int.Parse(Console.ReadLine());
-
-// Console.WriteLine($"Последняя цифра {number} = {number % 10}");
-
-
-Console.WriteLine("Введите число");
-int number = int.Parse(Console.ReadLine());
-int x = 2;
-while(x < number)
-{
-    Console.WriteLine(x);
-    x += 2;
-}
+// int x = 2;
+// while(x < number)
+// {
+//     Console.WriteLine(x);
+//     x += 2;
+// }
