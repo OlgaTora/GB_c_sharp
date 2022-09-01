7030e2305eb78302fe6a60426bf81542aeb94754
Olga Torres <bestlaly@mail.ru>
2022-09-01 13:50:35 +0300

    Lesson2

---
+++ Ex_011_method_array/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {10,82,73,24,35,56,27,27,8};
+
+int n = array.Length;
+int find = 27;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index ++;
+}
