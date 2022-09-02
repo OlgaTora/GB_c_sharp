8488c91bf5418b0ae3ce2ae5a8e47ab7f8a593eb
Olga Torres <bestlaly@mail.ru>
2022-09-02 22:15:24 +0300

    project 001 and todo init

---
+++ Seminar_2/project_001/Program.cs
@@ -0,0 +1,11 @@
+﻿void CompareDigits()
+    {
+    int x = new Random().Next(10, 100);
+    int x1 = x / 10;
+    int x2 = x % 10;
+    Console.WriteLine($"Сгенерировано число {x}");
+    if (x1 > x2) Console.WriteLine("Первое");
+    else if (x2 > x1) Console.WriteLine("Второе");
+    else Console.WriteLine("Палиндром");
+    }
+ CompareDigits();
