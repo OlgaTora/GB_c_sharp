55a62f607c04ba74d9a810fb59ebc125709b1ddc
Olga Torres <bestlaly@mail.ru>
2022-09-07 11:00:53 +0300

    todo 3 task added

---
+++ Seminar_3/project_001/Program.cs
@@ -0,0 +1,61 @@
+﻿//программа, которая по номеру четверти указывает диапазон возможных координат в этой
+//четверти (х и у).
+
+// string GetPointArea(int area)
+// {
+// if (area == 1) return "x>0, y>0";
+// if (area == 2) return "x<0, y>0";
+// if (area == 3) return "x<0, y<0";
+// if (area == 4) return "x>0, y<0";
+// return "Такой четверти нет";
+// }
+
+// Console.WriteLine("Input area: ");
+// int area = Convert.ToInt32(Console.ReadLine());
+
+// string result = GetPointArea(area);
+// Console.WriteLine(result);
+
+//ПРОграмма, которая принимает корр-ты двух точек в 2Д и выдает расстояние
+
+// double GetDistance(int x1, int x2, int y1, int y2)
+// {
+//     double distance = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+//     return distance;
+// }
+
+// Console.WriteLine("Input x1: ");
+// int x1 = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Input x2: ");
+// int x2 = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Input y1: ");
+// int y1 = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Input y2: ");
+// int y2 = int.Parse(Console.ReadLine());
+
+// double result = GetDistance(x1, x2, y1, y2);
+// Console.WriteLine(Math.Round(result, 2, MidpointRounding.AwayFromZero));
+
+ //Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+int [] GetSquareNumbers(int number)
+{
+    int [] squarenumbers = new int [number];
+
+    for (int i = 1; i <= number; i++)
+{
+squarenumbers[i - 1] = i * i;
+}
+return squarenumbers;
+}
+
+Console.WriteLine("Input number: ");
+int number = int.Parse(Console.ReadLine());
+
+int [] result = GetSquareNumbers(number);
+for (int i = 0; i < number; i++){
+    Console.WriteLine(result[i]);
+}
---
+++ Seminar_3/todo/Program.cs
@@ -0,0 +1,69 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет,
+//является ли оно палиндромом.
+// 14212 -> нет // 12821 -> да // 23432 -> да
+
+// void Palindrom(int x)
+// {
+//     if (x > 9999 & x < 100000)
+//         {
+//             if (x % 10 == x/10000 & (x % 100) / 10 == (x / 1000) % 10)
+//             {
+//                 Console.WriteLine("да");   
+//             }
+//         else Console.WriteLine("нет");
+//         }
+//     else Console.WriteLine("Неправильно введено число");
+// };
+
+// Console.WriteLine("Введите пятизначное число");
+// int x = Math.Abs(int.Parse(Console.ReadLine()));
+// Palindrom(x);
+
+// // Задача 21
+// // Напишите программу, которая принимает на вход координаты двух точек и
+// //находит расстояние между ними в 3D пространстве.
+// // A (3,6,8); B (2,1,-7), -> 15.84 // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// double GetDistance(int x1, int x2, int y1, int y2)
+// {
+//     double distance = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+//     return distance;
+// }
+
+// Console.WriteLine("Input x1: ");
+// int x1 = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Input x2: ");
+// int x2 = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Input y1: ");
+// int y1 = int.Parse(Console.ReadLine());
+
+// Console.WriteLine("Input y2: ");
+// int y2 = int.Parse(Console.ReadLine());
+
+// double result = GetDistance(x1, x2, y1, y2);
+// Console.WriteLine(Math.Round(result, 2, MidpointRounding.AwayFromZero));
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27  5 -> 1, 8, 27, 64, 125
+
+
+// int [] GetCubeNumbers(int number)
+// {
+//     int [] cubeNumbers = new int [number];
+// // fill array
+//     for (int i = 1; i <= number; i++)
+//         {
+//             cubeNumbers[i - 1] = Convert.ToInt32(Math.Pow(i, 3));
+//         }
+//     return cubeNumbers;
+// }
+
+// Console.WriteLine("Введите число: ");
+// int number = int.Parse(Console.ReadLine());
+
+// int [] result = GetCubeNumbers(number);
+// Console.WriteLine(string.Join(", ", result));
