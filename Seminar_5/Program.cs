27842bfe0fde5a276755e69531801c91195df164
Olga Torres <bestlaly@mail.ru>
2022-09-15 10:25:46 +0300

    Added Seminar_5 task 37

--- Seminar_5/Program.cs
+++ Seminar_5/Program.cs
@@ -5,7 +5,7 @@
 // int [] GetFilledArray(int size, int startValue, int endValue)
 // {
 //     var my_array = new int [size];
-//     // создаем объект, который имеет тип рандом
+//     // Создаем объект, который имеет тип рандом.
 //     var random = new Random();
 //     for (int i = 0; i < my_array.Length; i++)
 //     {
@@ -13,6 +13,7 @@
 //     }
 //     return my_array;
 // }
+
 
 // int CountPositiveNumbers(int [] my_array)
 // {
@@ -26,8 +27,9 @@
 //     }
 //     return count;
 // }
-// var my_array = GetFilledArray(12, 100, 1000);
-// //  Для примера беру size = 12
+
+// var my_array = GetFilledArray(12, 100, 999);
+// //  Для примера беру size = 12.
 // Console.WriteLine($"Random list is: [{string.Join(", ", my_array)}]");
 // int count = CountPositiveNumbers(my_array);
 // Console.WriteLine($"There are {count} positive numbers in random list");
@@ -36,35 +38,43 @@
 // Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19 ; [-4, -6, 89, 6] -> 0
 
-int [] GetFilledArray(int size, int startValue, int endValue)
-{
-    var my_array = new int [size];
-    // создаем объект, который имеет тип рандом
-    var random = new Random();
-    for (int i = 0; i < my_array.Length; i++)
-    {
-    my_array[i] = random.Next(startValue, endValue + 1);
-    }
-    return my_array;
-}
+// int [] GetFilledArray(int size, int startValue, int endValue)
+// {
+//     var my_array = new int [size];
+//     var random = new Random();
+//     for (int i = 0; i < my_array.Length; i++)
+//     {
+//     my_array[i] = random.Next(startValue, endValue + 1);
+//     }
+//     return my_array;
+// }
 
-int CountPositiveNumbers(int [] my_array)
-{
-    int count = 0;
-    for (int i = 0; i < my_array.Length; i++)
-    {
-        if (my_array[i] % 2 == 0)
-        {
-            count ++;
-        }
-    }
-    return count;
-}
-var my_array = GetFilledArray(12, 100, 1000);
-//  Для примера беру size = 12
-Console.WriteLine($"Random list is: [{string.Join(", ", my_array)}]");
-int count = CountPositiveNumbers(my_array);
-Console.WriteLine($"There are {count} positive numbers in random list");
+
+// int CountSumNegativeNumbers(int [] my_array)
+// {
+//     int sum = 0;
+//     for (int i = 1; i < my_array.Length; i+=2)
+//     {
+//         sum += my_array[i];
+//     }
+//     return sum;
+// }
+
+
+// int GetRandomSize ()
+// {
+//     var random = new Random();
+//     int size = random.Next(2, 20);
+//     // Прописываю рандомный размер массива.
+//     return size;
+// }
+
+// int size = GetRandomSize();
+// var my_array = GetFilledArray(size, -100, 100);
+// // Для примера используются числа в данном диапазоне
+// Console.WriteLine($"Random list is: [{string.Join(", ", my_array)}]");
+// int sum = CountSumNegativeNumbers(my_array);
+// Console.WriteLine($"The sum of numbers with negative index = {sum}");
 
 //Задача 37: Найдите произведение пар чисел в одномерном массиве.
 //Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
@@ -76,30 +86,101 @@ Console.WriteLine($"There are {count} positive numbers in random list");
 //      var random = new Random();
 //      for (int i = 0; i < myArray.Length; i++)
 //      {
-//      myArray[i] = random.Next(0, 100);
+//      myArray[i] = random.Next(-100, 100);
 //      }
 //      return myArray;
 // }
 
-// int [] GetMultyElements(int [] myArray, int size)
+
+// int [] GetEvenArray(int [] myArray)
+// // Метод для удаления центрального элемента из массива, чтобы он стал четным:
+// // так как в задаче не указано, как с ним поступить
 // {
-//     var newArray = new int [size];
-//     for (int i = 0; i < size; i++)
+//     var newArray = new int[myArray.Length - 1];
+//     var indexToDelete = myArray.Length / 2 - 1;
+
+//     for(int i = 0; i < indexToDelete; i++)
 //     {
-//         newArray[i] = myArray[i] * myArray[myArray.Length - i];
+//         newArray[i] = myArray[i];
+//     }
+
+//     for(int i = indexToDelete+ 1; i < newArray.Length; i++)
+//     {
+//         newArray[i] = myArray[i];
+//     }
+    
+//     return myArray;
+// }
+
+
+// int [] GetMultyElements(int [] myArray, int newSize)
+// {
+//     var newArray = new int [newSize];
+//     for (int i = 0; i < newSize; i++)
+//     {
+//         newArray[i] = myArray[i] * myArray[myArray.Length - i - 1];
 //     }
 // return newArray;
 // }
 
-// var myArray = GetFilledArray(10);
-// int size = myArray.Length / 2;
-// var newArray = GetMultyElements(myArray, size);
+
+// int GetRandomSize ()
+// {
+//     var random = new Random();
+//     int newSize = random.Next(2, 20);
+//     // Прописываю рандомный размер массива.
+//     return newSize;
+// }
+
+// int size = GetRandomSize();
+// var myArray = GetFilledArray(size);
 // Console.WriteLine($"First list is: [{string.Join(", ", myArray)}]");
-// Console.WriteLine($"Multiply list is: [{string.Join(", ", newArray)}]");
+// if (myArray.Length % 2 != 0) myArray = GetEvenArray(myArray);
+// int newSize = myArray.Length / 2;
+// var MultyArray = GetMultyElements(myArray, newSize);
+// Console.WriteLine($"Multiply list is: [{string.Join(", ", MultyArray)}]");
 
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 //[3 7 22 2 78] -> 76
 
+double [] GetFilledArray(int size,double startValue, double endValue)
+{
+    var my_array = new double [size];
+    var random = new Random();
+    for (int i = 0; i < my_array.Length; i++)
+    {
+    my_array[i] = random.Next(startValue, endValue + 1);
+    }
+    return my_array;
+}
+
+
+double SearchMax(double [] my_array)
+{
+    for (int i = 1; i < my_array.Length; i++)
+    {
+        double max = my_array[0];
+        if (max < my_array[i]) max = my_array[i];
+    }
+return max;
+}
+
+
+int GetRandomSize ()
+{
+    var random = new Random();
+    int size = random.Next(2, 20);
+    // Прописываю рандомный размер массива.
+    return size;
+}
+
+int size = GetRandomSize();
+var my_array = GetFilledArray(size, -100, 100);
+// Для примера используются числа в данном диапазоне
+Console.WriteLine($"Random list is: [{string.Join(", ", my_array)}]");
+double max = SearchMax(my_array);
+double min = SearchMin(my_array);
+Console.WriteLine($"Maximum of list = {max}, minimum = {min}");
 
 // Task 31 Задайте массив  из 12 элементов, заполненный случайными числами от -9 до 9. Найдите сумму + и - чисел
 
