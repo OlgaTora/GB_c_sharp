4052568f6e5dddccf4ae48569ec55082a61c9d5b
Olga Torres <bestlaly@mail.ru>
2022-09-28 13:45:55 +0300

    tasks 64, 66, 68

--- Seminar_9/Program.cs
+++ Seminar_9/Program.cs
@@ -1,24 +1,101 @@
-﻿Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа
+//в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-N = 5 -> "5, 4, 3, 2, 1"
-N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+// string GetNumbers(int startNumber)
+// {
+//     if (startNumber > 1) return $"{startNumber}" + ", " + GetNumbers(startNumber - 1);
+//     else return "1";
+// }
 
-Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// try
+// {
+//     Console.Write("Input N > 0: ");
+//     int startNumber = int.Parse(Console.ReadLine());
+//     if (startNumber < 0) Console.WriteLine("Error! You have to input natural number!");
+//     else Console.WriteLine(GetNumbers(startNumber));
+// }
+//  catch (Exception ex)
+// {
+//      Console.WriteLine("Error! You have to input natural number!");
+// }
 
-M = 1; N = 15 -> 120
-M = 4; N = 8. -> 30
 
-Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
-m = 2, n = 3 -> A(m,n) = 9
-m = 3, n = 2 -> A(m,n) = 29
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов
+//в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// int GetSum(int startNumber, int endNumber)
+// {
+//     int sum = 0;
+//     if (startNumber == endNumber) sum = startNumber;
+//     else
+//     {
+//         // В задании не указано, какое число больше 
+//         if (startNumber < endNumber)
+//         {
+//             sum = endNumber + GetSum(startNumber, endNumber - 1);
+//         }
+//         else sum = startNumber + GetSum(startNumber - 1, endNumber);
+//     }
+//     return sum;
+// }
+
+// try
+// {
+//     Console.Write("Input M > 0: ");
+//     int startNumber = int.Parse(Console.ReadLine());
+//     Console.Write("Input N > 0: ");
+//     int endNumber = int.Parse(Console.ReadLine());
+//     if (startNumber < 0 || endNumber < 0) Console.WriteLine("Error! You have to input natural number!");
+//     else
+//     {
+//         int sum = GetSum(startNumber, endNumber);
+//         Console.WriteLine($"Sum is: {sum}");
+//     }
+// }
+
+//  catch (Exception ex)
+// {
+//      Console.WriteLine("Error! You have to input natural number!");
+// }
 
 
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+//Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+// int Ackermann(int numberM, int numberN)
+// {
+// if (numberM == 0) return numberN + 1;
+// else if (numberM > 0 && numberN == 0) return Ackermann(numberM - 1, 1);
+// else return Ackermann(numberM - 1, Ackermann(numberM, numberN - 1));
+// }
+
+// try
+// {
+//     Console.Write("Input M > 0: ");
+//     int numberM = int.Parse(Console.ReadLine());
+//     Console.Write("Input N > 0: ");
+//     int numberN = int.Parse(Console.ReadLine());
+//     if (numberM < 0 || numberN < 0) Console.WriteLine("Error! You have to input natural number!");
+//     else
+//     {
+//         int aсkermann = Ackermann(numberM, numberN);
+//         Console.WriteLine($"Ackermann function result is: {aсkermann}");
+//     }
+// }
+
+//  catch (Exception ex)
+// {
+//      Console.WriteLine("Error! You have to input natural number!");
+// }
 
 
 // Task 63. Все натуральные числа между 1 и N.
-
-// Console.WriteLine("Input N > 0: ");
-// int endNumber = int.Parse(Console.ReadLine());
 
 // string GetNaturalNumbers(int endNumber)
 // {
@@ -27,7 +104,6 @@ m = 3, n = 2 -> A(m,n) = 29
 //     else natNum = GetNaturalNumbers(endNumber - 1) +"," + $"{endNumber}";
 //     return natNum;
 // }     
-// Console.WriteLine(GetNaturalNumbers(endNumber));
 
 // int GetSumNumbers(int endNumber)
 // {
@@ -36,25 +112,40 @@ m = 3, n = 2 -> A(m,n) = 29
 //     sum = GetSumNumbers(endNumber - 1) + endNumber;
 //     return sum;
 // }
-// int sum = GetSumNumbers(endNumber);
-// Console.WriteLine(sum);
 
-//Task 65. Все натуральные числа между  N и M.
+// try
+// {
+//     Console.Write("Input N > 0: ");
+//     int endNumber = int.Parse(Console.ReadLine());
+//     if (endNumber < 0) Console.WriteLine("Error! You have to input natural number!");
+//     else
+//     {
+//         Console.WriteLine(GetNaturalNumbers(endNumber));
+//         int sum = GetSumNumbers(endNumber);
+//         Console.WriteLine($"Sum is: {sum}");
+//     }
+// }
+//  catch (Exception ex)
+// {
+//      Console.WriteLine("Error! You have to input natural number!");
+// }
 
-Console.WriteLine("Input N > 0: ");
-int startNumber = int.Parse(Console.ReadLine());
-Console.WriteLine("Input M > 0: ");
-int endNumber = int.Parse(Console.ReadLine());
+//Task 65. Все натуральные числа между  N и M, их сумма.
 
-string GetNaturalNumbers(int startNumber, int endNumber)
-{
-    string natNum = "";
-    if (endNumber == startNumber) natNum = $"{startNumber}";
-    else natNum = GetNaturalNumbers(startNumber, endNumber - 1) + ", " + $"{endNumber}";
-    return natNum;
-}
+// Console.WriteLine("Input N > 0: ");
+// int startNumber = int.Parse(Console.ReadLine());
+// Console.WriteLine("Input M > 0: ");
+// int endNumber = int.Parse(Console.ReadLine());
 
-Console.WriteLine(GetNaturalNumbers(startNumber, endNumber));
+// string GetNaturalNumbers(int startNumber, int endNumber)
+// {
+//     string natNum = "";
+//     if (endNumber == startNumber) natNum = $"{startNumber}";
+//     else natNum = GetNaturalNumbers(startNumber, endNumber - 1) + ", " + $"{endNumber}";
+//     return natNum;
+// }
+
+// Console.WriteLine(GetNaturalNumbers(startNumber, endNumber));
 
 // int GetSumNumbers(int endNumber)
 // {
