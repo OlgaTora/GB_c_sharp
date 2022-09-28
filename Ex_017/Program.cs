31090705a62755da6f237b41426a7a032cdae9d7
Olga Torres <bestlaly@mail.ru>
2022-09-28 12:32:03 +0300

    added lesson

---
+++ Ex_017/Program.cs
@@ -0,0 +1,17 @@
+﻿// Собрать строку с числами от a до b, a ≤ b.
+string NumbersFor(int a, int b)
+{
+ string result = String.Empty;
+ for (int i = a; i <= b; i++)
+ {
+ result += $"{i} ";
+ }
+ return result;
+}
+string NumbersRec(int a, int b)
+{
+ if (a <= b) return $"{a} " + NumbersRec(a + 1, b);
+ else return String.Empty;
+}
+
+
